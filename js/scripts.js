58b83b5d5f82e0d81ebefd3799296242fba63d0b
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2022-11-01 09:39:17 +0200

    Сompleted the project

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$( function() {
+    $( "#accordion" ).accordion();
+} );
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        768:{
+            items:2
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
